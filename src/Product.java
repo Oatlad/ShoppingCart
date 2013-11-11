319e4ebfab10d20367cfd6c240aa5d3470053cf3
grahambond <grahambond@smmgraham.netgear.com>
2013-11-10 17:28:38 -0700

    Test message

---
+++ src/Product.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author grahambond
+ */
+public class Product {
+
+    //super class object properties
+    private int productCode;
+    private String description;
+    private int avgRating;
+    private double price;
+    private int inventory;
+
+    public int getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(int productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAvgRating() {
+        return avgRating;
+    }
+
+    public void setAvgRating(int avgRating) {
+        this.avgRating = avgRating;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    //consructor
+    
+    
+    public Product() {
+        this.avgRating = avgRating;
+        this.description = description;
+        this.inventory = inventory;
+        this.price = price;
+        this.productCode = productCode;
+    }
+    
+    
+    
+    public Product(int productCode, String description, int avgRating, double price, int inventory) {
+        this.productCode = productCode;
+        this.description = description;
+        this.avgRating = avgRating;
+        this.price = price;
+        this.inventory = inventory;
+    }
+
+    @Override
+    public String toString() {
+        return "You have selected: " + getDescription() + "(PC)" + getProductCode() + "\n It costs: " + getPrice() +
+               "The average customer rating is: " + getAvgRating() + "\n  There are: " + getInventory() + " left in stock";
+                
+    }
+}
