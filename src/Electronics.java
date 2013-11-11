c76c20a12c042acf2f8c44d3bf2e7685f7891523
grahambond <grahambond@smmgraham.netgear.com>
2013-11-10 17:28:53 -0700

    Test message

---
+++ src/Electronics.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author grahambond
+ */
+public class Electronics extends Product {
+    
+    private String manufacturer;
+    private String modelNum;
+    private int serialNum;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModelNum() {
+        return modelNum;
+    }
+
+    public void setModelNum(String modelNum) {
+        this.modelNum = modelNum;
+    }
+
+    public int getSerialNum() {
+        return serialNum;
+    }
+
+    public void setSerialNum(int serialNum) {
+        this.serialNum = serialNum;
+    }
+
+    public Electronics(String manufacturer, String modelNum, int serialNum) {
+        this.manufacturer = manufacturer;
+        this.modelNum = modelNum;
+        this.serialNum = serialNum;
+    }
+
+    public Electronics(String manufacturer, String modelNum, int serialNum, int productCode, String description, int avgRating, double price, int inventory) {
+        super(productCode, description, avgRating, price, inventory);
+        this.manufacturer = manufacturer;
+        this.modelNum = modelNum;
+        this.serialNum = serialNum;
+    }
+
+    public Electronics() {
+        this.manufacturer = manufacturer;
+        this.modelNum = modelNum;
+        this.serialNum = serialNum;
+    }
+    
+    
+    
+    @Override 
+    public String toString() {
+        return "";
+    }
+    
+    
+    
+}
